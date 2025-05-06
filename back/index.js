29a677be8848a7edbe4782f650c390eadeb2e1db
profuentes2022 <100309580+profuentes2022@users.noreply.github.com>
2025-05-06 17:11:14 +0200

    db y server
    creacion de  la base de datos tiendonode
    y el servidor en el puerto  5555

---
+++ back/index.js
@@ -0,0 +1,9 @@
+const express=require("express");
+const mysql=require("mysql");
+
+const app=express();
+
+
+app.listen(5555,()=>{
+    console.log("seridor en linea ");
+});
--- bd/script.sql
+++ bd/script.sql
@@ -1,14 +1,14 @@
+CREATE USER 'backappnode'@'localhost' IDENTIFIED BY 'node123';
+-- Grant select privilege to all databases;
+GRANT SELECT ON *.* TO 'backappNode'@'localhost' WITH GRANT OPTION;
+-- Grant all privileges to all databases;
+GRANT ALL PRIVILEGES ON *.* TO 'backappNode'@'localhost' WITH GRANT OPTION;
 
 
-
-create database proyecto;
-
-use proyecto;
-
-create table usuarios(
-    id int auto_increment primary key,
-    nombre varchar(25),
-    clave varchar(25),
-   
-    rol varchar(25)
-)
+CREATE TABLE usuarios(  
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+    create_time DATETIME COMMENT 'Create Time',
+    usuario VARCHAR(50),
+    clave VARCHAR(50),
+    rol varchar(50)
+)engine=innodb;
